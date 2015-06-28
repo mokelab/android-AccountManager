00acd2b41c0517320ff7cc2c43b06ebdb151d88e
hrk fkm <fkm@fkmsoft.jp>
2015-06-28 23:18:23 +0900

    Add authenticator and login activity

---
+++ app/src/main/java/com/mokelab/accountsample/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.mokelab.accountsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity for login page
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
