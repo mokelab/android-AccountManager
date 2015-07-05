bea4e9f8cfbea4881b718df8b8e446ccd6eee271
hrk fkm <fkm@fkmsoft.jp>
2015-07-05 22:40:44 +0900

    Add SyncAdapter class

---
+++ app/src/main/java/com/mokelab/accountsample/sync/SyncAdapter.java
@@ -0,0 +1,35 @@
+package com.mokelab.accountsample.sync;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Sync adapter
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+    private ContentResolver mContentResolver;
+
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+
+        mContentResolver = context.getContentResolver();
+    }
+
+    public SyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+
+        mContentResolver = context.getContentResolver();
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle bundle, String s,
+                              ContentProviderClient contentProviderClient, SyncResult syncResult) {
+
+    }
+}
